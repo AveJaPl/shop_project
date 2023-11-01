b786a2b2e262192ac89e1705890a7c1df52dd11a
Piatekf <filip.piatek2005@gmail.com>
2023-11-01 14:47:26 +0100

    uninstalled graphQL

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,8 @@
-import { ApolloServer } from "@apollo/server";
 import express from "express";
 
 const app = express();
 const PORT = 4000;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
