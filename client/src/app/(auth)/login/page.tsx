ad421a894b4913c6e51eece7a038e76cc8cb554e
Piatekf <filip.piatek2005@gmail.com>
2023-11-02 17:17:07 +0100

    dashboard register and login pages

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,8 +1,99 @@
+"use client";
 
+import React, { useState } from "react";
+import { loginUser } from "@/services/login";
+import { useRouter } from 'next/navigation'
 const Login = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string
+        | null>(null);
+
+    const [auth, setAuth] = useState(false);
+    const [token, setToken] = useState("");
+
+    const router = useRouter();
+
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await loginUser(email, password)
+
+            if (response.status === 200) {
+                console.log("token:", response.data.token);
+                setToken(response.data.token);
+                setAuth(true);
+                router.push('/dashboard');
+            } else {
+                setError(response.data.message || 'An error occurred');
+            }
+        } catch (err: any) {
+            setError(err.message || 'An error occurred');
+        } finally {
+            setLoading(false);
+        }
+
+
+    }
+
+
+    const handleForgotPassword = () => {
+
+    }
+
+
+
+
     return (
-        <div>
-            Login
+        <div className="w-full h-full flex justify-center items-center bg-gray-200">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+                {error && <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4">{error}</div>}
+                <form onSubmit={handleLogin} className="space-y-4">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md text-black-500"
+                            required
+                        />
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <button
+                            type="submit"
+                            className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded"
+                            disabled={loading}
+                        >
+                            {loading ? "Logging in..." : "Login"}
+                        </button>
+                        <a href="#" onClick={handleForgotPassword} className="text-blue-500 hover:underline text-sm">Forgot password?</a>
+                    </div>
+                </form>
+                <div className="mt-4">
+                    <span className="text-sm text-gray-600">Don't have an account? </span>
+                    <a href="/register" className="text-blue-500 hover:underline text-sm">Register</a>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,9 +1,105 @@
+"use client";
+
+import React, { useState } from "react";
+import { registerUser } from "@/services/register";
+import { useRouter } from 'next/navigation'
+
 const Register = () => {
+    const [name, setName] = useState<string>("");
+    const [surname, setSurname] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const router = useRouter();
+
+    const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await registerUser(name, surname, email, password);
+            console.log(response.data);
+            router.push('/dashboard');
+        } catch (err: any) {
+            setError(err.message || 'An error occurred');
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <div>
-            <h1>Register Page</h1>
-        </div>
+        <div className="w-full h-full flex justify-center items-center bg-gray-200">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h1 className="text-2xl font-bold mb-6 text-center">Register</h1>
+                {error && <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4">{error}</div>}
+                <form onSubmit={handleRegister} className="space-y-4">
+                    <div>
+                        <label htmlFor="name" className="block text-sm font-medium text-gray-600">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="surname" className="block text-sm font-medium text-gray-600">Surname</label>
+                        <input
+                            type="text"
+                            name="surname"
+                            id="surname"
+                            value={surname}
+                            onChange={e => setSurname(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <button
+                            type="submit"
+                            className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded"
+                            disabled={loading}
+                        >
+                            {loading ? "Logging in..." : "Login"}
+                        </button>
+                    </div>
+                </form>
+                <div className="mt-4">
+                    <span className="text-sm text-gray-600">Already have an account? </span>
+                    <a href="/login" className="text-blue-500 hover:underline text-sm">Login</a>
+                </div>
+            </div>
+        </div >
     )
 }
 
-export default Register
+export default Register;
