768884715d66dab0b28a8fd96160d27d0f098450
Piatekf <filip.piatek2005@gmail.com>
2023-11-02 17:16:35 +0100

    font awesome icon component

--- client/src/components/common/userIcon.tsx
+++ client/src/components/common/userIcon.tsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import IUserIconProps from "@/types/IUserIconProps";
 
-const UserIcon = () => {
+const UserIcon: React.FC<IUserIconProps> = ({ className = '' }) => {
   return (
     <Link href="/login">
-      <FontAwesomeIcon className=" w-4" icon={faUser} />
+      <FontAwesomeIcon className={className} icon={faUser} />
     </Link>
   );
 };
