6a987d65c9881dbfb7adc66d446fb6b55b19d3f3
Piatekf <filip.piatek2005@gmail.com>
2023-11-04 21:03:20 +0100

    optional id in product inteface

--- client/src/types/product.ts
+++ client/src/types/product.ts
@@ -1,13 +1,13 @@
 interface Product {
-    id: number;
-    name: string;
-    price: number;
-    image?: string;
-    description?: string;
-    category?: string;
-    countInStock?: number;
-    rating?: number;
-    numReviews?: number;
+  id?: number;
+  name: string;
+  price: number;
+  image?: string;
+  description?: string;
+  category?: string;
+  countInStock?: number;
+  rating?: number;
+  numReviews?: number;
 }
 
 export default Product;
