0193a46610b92261953dbe122ef320a5d8c5d228
Piatekf <filip.piatek2005@gmail.com>
2023-11-05 09:38:43 +0100

    product service connected with backend

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,35 +1,35 @@
 import axios from 'axios';
-import Product from '@/types/product';
+import { IAddedProduct, IModifiedProduct, Product } from '@/types/product';
 import Category from '@/types/Category';
 
 const getAllProducts = async (): Promise<Product[]> => {
-    const response = await axios.get('/api/products');
-    return response.data;
+    const response = await axios.get('http://localhost:4000/products');
+    return response.data as Product[]
 }
 
-const AddProduct = async (product: Product): Promise<Product> => {
+const AddProduct = async (product: IAddedProduct): Promise<Product> => {
     const response = await axios.post('http://localhost:4000/products/', product);
-    return response.data;
+    return response.data as Product
 }
 
 const DeleteProduct = async (id: number): Promise<Product> => {
-    const response = await axios.delete(`/api/products/${id}`);
-    return response.data;
+    const response = await axios.delete(`http://localhost:4000/products/${id}`);
+    return response.data as Product
 }
 
-const UpdateProduct = async (oldProductId: number, product: Product): Promise<Product> => {
-    const response = await axios.put(`/api/products/${oldProductId}`, product);
-    return response.data;
+const UpdateProduct = async (oldProductId: number, product: IModifiedProduct): Promise<Product> => {
+    const response = await axios.put(`http://localhost:4000/products/${oldProductId}`, product);
+    return response.data as Product
 }
 
-const getAllCategories = async (): Promise<Category[]> => {
-    const response = await axios.get('/api/products/categories');
+const GetAllCategories = async (): Promise<Category[]> => {
+    const response = await axios.get('http://localhost:4000/category');
     return response.data as Category[];
 }
 
-const AddCategory = async (category: {name: string}): Promise<{id: number}> => {
-    const response = await axios.post('/api/products/categories', category);
-    return response.data;
+const GetCategoryById = async(categoryId: number): Promise<Category>=>{
+    const response = await axios.get(`http://localhost:4000/category/${categoryId}`)
+    return response.data as Category
 }
 
 export {
@@ -37,6 +37,6 @@ export {
     AddProduct,
     DeleteProduct,
     UpdateProduct,
-    getAllCategories,
-    AddCategory
+    GetAllCategories,
+    GetCategoryById
 }
