f4aeb3a2c0fd8ba836d7f4592f638b4547cc22d4
Piatekf <filip.piatek2005@gmail.com>
2023-11-05 13:09:08 +0100

    product card changes

--- client/src/components/common/productCard.tsx
+++ client/src/components/common/productCard.tsx
@@ -1,20 +1,30 @@
 // components/ProductCard.tsx
 import React from "react";
 import Image from "next/image";
-import Product from "../../types/product";
+import { Product } from "../../types/product";
+import { useRouter } from "next/navigation";
 
 const ProductCard: React.FC<Product> = ({ ...product }) => {
-const handleClick = (e: any) => {
-    console.log("klik", e.target)
-}
+  const router = useRouter();
+
+  const createSlug = (name: string, id:number) => {
+    return name.toLowerCase().split(" ").join("-") + "-" + id;
+  }
+
+
+  const handleClick = () => {
+    const slug = createSlug(product.name, product.id)
+    router.push(`/products/${slug}`);
+  };
   return (
-    <div onClick={e=>handleClick(e)} className="border p-4 rounded-lg">
+    <div onClick={handleClick} className=" mx-1.5 border px-4 py-2 rounded-lg">
       <Image
-        src={product.image || "/placeholder.png"}
+        src="/placeholder.png"
         alt={product.name}
         width={500}
         height={500}
         className="lg:w-full md:w-full sm:w-1/2 h-48 object-cover rounded-t-lg"
+        priority={true}
       />
       <h3 className="text-lg font-bold mt-2">{product.name}</h3>
       <p className="text-md">{product.price} zł</p>
