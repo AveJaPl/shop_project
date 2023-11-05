bc6107686982cfb09c31180a2eecf80a432d096b
Piatekf <filip.piatek2005@gmail.com>
2023-11-05 13:06:43 +0100

    slider carousel implementation

--- client/src/components/layout/bestsellers.tsx
+++ client/src/components/layout/bestsellers.tsx
@@ -2,42 +2,28 @@
 
 import React, {useState, useEffect} from 'react';
 import ProductCard from '../common/productCard';
-import Product from '../../types/product';
+import {Product} from '../../types/product';
+import carouselSettings from '../common/carouselSettings';
+import { getBestSellerProducts } from '@/services/ProductService';
+
+import Slider from 'react-slick';
 const FeaturedProducts = () => {
   const [bestsellers, setBestsellers] = useState<Product[]>([])
   useEffect(() => {
-      const fetchNewArrivals = async () => {
-          setBestsellers([
-              {
-                  id: 1,
-                  name: "Produkt 1",
-                  price: 100,
-                  
-              },
-              {
-                  id: 2,
-                  name: "Produkt 2",
-                  price: 200,
-                  
-              },
-              {
-                  id: 3,
-                  name: "Produkt 3",
-                  price: 300,
-                  
-              }
-
-          ])
-
+      const fetchBestsellers = async () => {
+          const response = await getBestSellerProducts()
+          setBestsellers(response)
       }
-      fetchNewArrivals()
+      fetchBestsellers()
   }, [])
   return (
     <section>
       <h2 className="text-2xl font-bold my-6">Bestsellers</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {bestsellers.map(product => <ProductCard key={product.id} {...product} />)}
-      </div>
+      <Slider {...carouselSettings}>
+        {bestsellers.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </Slider>
     </section>
   );
 };
--- client/src/components/layout/newarrivals.tsx
+++ client/src/components/layout/newarrivals.tsx
@@ -1,79 +1,36 @@
-"use client"
+"use client";
 
-import React, {useEffect, useState} from 'react'
-import ProductCard from '../common/productCard'
-import Product from '../../types/product'
-
-
+import React, { useEffect, useState } from "react";
+import ProductCard from "../common/productCard";
+import { Product } from "../../types/product";
+import { getNewArrivalProducts } from "@/services/ProductService";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import carouselSettings from "@/components/common/carouselSettings";
 function NewArrivals() {
-    const [newArrivals, setNewArrivals] = useState<Product[]>([])
-    useEffect(() => {
-        const fetchNewArrivals = async () => {
-            setNewArrivals([
-                {
-                    id: 1,
-                    name: "Produkt 1",
-                    price: 100,
-                    
-                },
-                {
-                    id: 2,
-                    name: "Produkt 2",
-                    price: 200,
-                    
-                },
-                {
-                    id: 3,
-                    name: "Produkt 3",
-                    price: 300,
-                    
-                },
-                {
-                    id: 4,
-                    name: "Produkt 4",
-                    price: 400,
-                },
-                {
-                    id: 5,
-                    name: "Produkt 5",
-                    price: 500,
-                },
-                {
-                    id: 6,
-                    name: "Produkt 6",
-                    price: 600,
-                },
-                {
-                    id: 7,
-                    name: "Produkt 7",
-                    price: 700,
-                },
-                {
-                    id: 8,
-                    name: "Produkt 8",
-                    price: 800,
-                },
-                {
-                    id: 9,
-                    name: "Produkt 9",
-                    price: 900,
-                }
+  
 
-            ])
-
-        }
-        fetchNewArrivals()
-    }, [])
+  const [newArrivals, setNewArrivals] = useState<Product[]>([]);
+  useEffect(() => {
+    const fetchNewArrivals = async () => {
+      const response = await getNewArrivalProducts();
+      console.log("odpowiedz", response);
+      setNewArrivals(response);
+    };
+    fetchNewArrivals();
+  }, []);
 
   return (
     <section>
-        <h2 className='text-2xl font-bold my-6'>New Arrivals</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {newArrivals.map(product => <ProductCard key={product.id} {...product}/>)}
-
-        </div>
+      <h2 className="text-2xl font-bold my-6">New Arrivals</h2>
+      <Slider {...carouselSettings}>
+        {newArrivals.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </Slider>
     </section>
-  )
+  );
 }
 
-export default NewArrivals
+export default NewArrivals;
