fb57a29a5a98fb4511a0290623d4430fca6cf30b
Piatekf <filip.piatek2005@gmail.com>
2023-11-09 19:08:21 +0100

    better looking footer

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -1,18 +1,25 @@
+// Updated Footer component with a more modern design
 const Footer = () => {
   return (
-    <footer className="w-full p-4 bg-indigo-700">
-      <div className="flex justify-center">
-        <p className="text-white text-sm">
-          © {new Date().getFullYear()}{" "}
+    <footer className="w-full bg-indigo-700">
+      <div className="max-w-screen-xl mx-auto px-4 py-6 flex justify-between items-center flex-col sm:flex-row">
+        <div className="text-white text-sm text-center sm:text-left flex flex-col sm:flex-row sm:items-center gap-2">
+          <span>© {new Date().getFullYear()} Avejapl.</span>
           <a
             href="https://github.com/avejapl"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-indigo-200 font-bold hover:text-indigo-400 transition duration-300"
+            className="font-bold hover:text-indigo-400 transition duration-300"
           >
-            Avejapl
+            GitHub
           </a>
-        </p>
+          <span>| All rights reserved.</span>
+        </div>
+        <div className="mt-4 sm:mt-0 flex gap-4">
+          <a href="#" className="text-indigo-200 hover:text-white transition duration-300">Privacy Policy</a>
+          <a href="#" className="text-indigo-200 hover:text-white transition duration-300">Terms of Service</a>
+          <a href="#" className="text-indigo-200 hover:text-white transition duration-300">Support</a>
+        </div>
       </div>
     </footer>
   );
