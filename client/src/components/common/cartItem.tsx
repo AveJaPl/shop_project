cd82867191ef46e9047cc7f8c087c6008fe99a04
Piatekf <filip.piatek2005@gmail.com>
2023-11-12 23:59:05 +0100

    cart item created

---
+++ client/src/components/common/cartItem.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import { CartProduct } from "@/types/product";
+import { removeFromCart } from "@/services/cart";
+
+type CartItemProps = {
+  item: CartProduct;
+};
+
+const CartItem: React.FC<CartItemProps> = ({ item }) => {
+
+  const handleRemove = async() => {
+    try{
+      const response = await removeFromCart(item.id);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  return (
+    <div className="flex justify-between items-center border-b border-gray-200 py-4">
+      <div className="flex items-center">
+        <Image
+          src="/placeholder.png"
+          width={80}
+          height={80}
+          alt={item.name}
+          className="h-20 w-20 object-cover mr-4"
+        />
+        <div>
+          <p className="text-lg font-semibold">{item.name}</p>
+          <p className="text-sm text-gray-600">Quantity: {item.quantity}</p>
+        </div>
+        <button onClick={handleRemove}>Remove</button>
+      </div>
+      <span className="text-lg font-semibold">{item.price} zł</span>
+    </div>
+  );
+};
+
+export default CartItem;
