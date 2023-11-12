78e7686a660b9fa0b4da5b3b521a7724d299b2ee
Piatekf <filip.piatek2005@gmail.com>
2023-11-13 00:00:32 +0100

    addToCart function added

--- client/src/components/layout/fullproductpage.tsx
+++ client/src/components/layout/fullproductpage.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Product } from '@/types/product';
 import Image from 'next/image';
+import { addToCart } from '@/services/cart';
 
 const FullProductPage: React.FC<{ product: Product }> = ({product }) => {
   const inStock = product.countInStock > 0;
   const stockIndicatorColor = inStock ? 'text-green-500' : 'text-red-500';
   const stockText = inStock ? 'In Stock' : 'Out of Stock';
   const isLongName = product.name.length > 20;
+
+  const handleAddToCart = async() =>{
+    try{
+      const cart = await addToCart(product.id);
+      console.log(cart);
+    }catch(error){
+      console.log(error);
+    }
+
+  }
+
   return (
-    <div className="w-2/3 mx-auto p-4 flex flex-col justify-center items-center">
+    <div className="sm:w-3/4 md:w-2/3 lg:w-3/5 xl:w-2/5 mx-auto p-4 flex flex-col justify-center items-center">
       <div className="w-full bg-white shadow-lg overflow-hidden md:flex">
 
-        <div className="md:w-1/2">
+        <div className="md:w-1/2 sm:flex sm:justify-center xs:flex xs:justify-center">
           <Image
             src='/placeholder.png'
             alt={product.name}
@@ -34,6 +46,7 @@ const FullProductPage: React.FC<{ product: Product }> = ({product }) => {
 
           </div>
           <button
+          onClick={handleAddToCart}
             className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out ${!inStock && 'opacity-50 cursor-not-allowed'}`}
             disabled={!inStock}
           >
